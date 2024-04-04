43c770f4bf7b937c740a45fa12aa2a60be8319c9
gradan-hash <mutukunyaa@gmail.com>
2024-04-04 13:28:46 +0300

    charts

--- controllers/Admin/Orders.js
+++ controllers/Admin/Orders.js
@@ -11,3 +11,29 @@ export const GetAllOrders = async (req, res, next) => {
     next(error);
   }
 };
+
+export const Charts = async (req, res, next) => {
+  try {
+    const allOrders = await Order.find({});
+
+    // Initialize an array to store chart data
+    const chartData = [];
+
+    // Iterate over each order
+    allOrders.forEach((order) => {
+      // Get the day the order was updated
+      const day = new Date(order.updatedAt).toLocaleDateString();
+
+      // Push the order's amountPaid and day to the chartData array
+      chartData.push({
+        day,
+        amountPaid: order.amountPaid,
+      });
+    });
+
+    res.status(200).json(chartData);
+  } catch (error) {
+    console.error("Failed to fetch chart data:", error);
+    next(error);
+  }
+};
--- routes/Admin/Orders.js
+++ routes/Admin/Orders.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { GetAllOrders } from "../../controllers/Admin/Orders.js";
+import { Charts, GetAllOrders } from "../../controllers/Admin/Orders.js";
 import { VerifyToken } from "../../middlewares/jwt.js";
 const router = express.Router();
 
 router.get("/allordersadmin", GetAllOrders);
+router.get("/charts", Charts);
 
 export default router;
