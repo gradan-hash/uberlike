76fab4ef9517b52760ebd8064d29775db773cae8
gradan-hash <mutukunyaa@gmail.com>
2024-04-04 09:08:18 +0300

    Well well

--- controllers/Admin/Client.js
+++ controllers/Admin/Client.js
@@ -16,7 +16,8 @@ export const GetAllUsers = async (req, res, next) => {
 };
 export const DeleteUser = async (req, res, next) => {
   try {
-    const userId = req.params.id; // Assuming you're passing the user ID in the URL
+    const userId = req.params.userId; // Assuming you're passing the user ID in the URL
+    console.log(userId);
     const deletedUser = await user.findByIdAndDelete(userId);
     if (!deletedUser) throw createError(404, "User not found");
 
--- routes/Admin/Client.js
+++ routes/Admin/Client.js
@@ -6,7 +6,7 @@ import { DeleteUser, GetAllUsers } from "../../controllers/Admin/Client.js";
 const router = express.Router();
 
 router.get("/allusers", VerifyToken, GetAllUsers);
-router.get("/delete/:userId", VerifyToken, DeleteUser);
+router.delete("/delete/:userId", VerifyToken, DeleteUser);
 
 // router.post("/accesstoken",VerifyToken)
 
